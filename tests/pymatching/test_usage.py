096e059a089b01bffffda0b43bc1ab485b9dc6d1
ehua7365 <ehua7365@users.noreply.github.com>
2021-03-24 08:36:22 +1100

    [Add] test from usage tutorial of pymatching

--- tests/pymatching/test_usage.py
+++ tests/pymatching/test_usage.py
@@ -1,16 +1,49 @@
+"""
+Following the tutorial.
+"""
+import pytest
 from pymatching import Matching
-
 import numpy as np
+import scipy
 
-def test_getting_started():
 
-    # Five-qubit bit flip code.
+@pytest.fixture
+def five_qubit_Hz():
+    """Five-qubit bit flip code parity check matrix."""
     Hz = np.array([
         [1, 1, 0, 0, 0],
         [0, 1, 1, 0, 0],
         [0, 0, 1, 1, 0],
         [0, 0, 0, 1, 1],
     ])
+    return Hz
+
+
+def test_getting_started(five_qubit_Hz):
+    """From the tutorial."""
+
+    Hz = five_qubit_Hz
+    m = Matching(Hz)
+
+    # Noise example IIXXI.
+    noise = np.array([0, 0, 1, 1, 0])
+
+    # Syndrome vector.
+    z = Hz.dot(noise) % 2
+    assert np.all(z == [0, 1, 0, 1])
+
+    # Decode the syndrome.
+    c = m.decode(z)
+
+    assert type(c) is np.ndarray
+    assert np.all(c == [0, 0, 1, 1, 0])
+
+
+def test_getting_started_sparse(five_qubit_Hz):
+    """Sparse version."""
+
+    # Sparse version.
+    Hz = scipy.sparse.csr_matrix(five_qubit_Hz)
 
     m = Matching(Hz)
 
@@ -24,7 +57,5 @@ def test_getting_started():
     # Decode the syndrome.
     c = m.decode(z)
 
-    print(f'C: {c}, of type {type(c)}')
     assert type(c) is np.ndarray
-
     assert np.all(c == [0, 0, 1, 1, 0])
