e7146f8e29dd5ad691577439db7568b558457bb6
ehua7365 <ehua7365@users.noreply.github.com>
2021-04-07 07:00:04 +1000

    [Add] function for plotting saved json file

---
+++ bn3d/plots.py
@@ -0,0 +1,31 @@
+import json
+import matplotlib.pyplot as plt
+
+
+def plot_export_json(export_json):
+    """Plot data in previously exported .json file."""
+    with open(export_json) as f:
+        data = json.load(f)
+
+    plt.figure(figsize=(16, 9))
+    for i, L in enumerate(data['parameters']['L_list']):
+        plt.errorbar(
+            data['parameters']['p_list'],
+            data['statistics']['p_est'][i],
+            yerr=data['statistics']['p_se'][i],
+            label=f'L={L}'
+        )
+    plt.xlabel('Physical Error Rate', fontsize=20)
+    plt.ylabel('Logical Error Rate', fontsize=20)
+    time_elapsed = data['time']['time_elapsed']
+    time_remaining = data['time']['time_remaining']
+    eta = data['time']['eta']
+    i_trial = data['parameters']['i_trial']
+    n_trials = data['parameters']['n_trials']
+    plt.title(
+        f'Progress {100*(i_trial + 1)/n_trials}%, '
+        f'{i_trial + 1} out of {n_trials}\n'
+        f'Elapsed {time_elapsed}, Remaining {time_remaining}\n'
+        f'ETA {eta}'
+    )
+    plt.legend()
