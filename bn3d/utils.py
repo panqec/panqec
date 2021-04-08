48e8bc711b64abf803ed500102483a897f948e62
ehua7365 <ehua7365@users.noreply.github.com>
2021-04-08 13:34:37 +1000

    [Add] function for nested map on lists of lists

--- bn3d/utils.py
+++ bn3d/utils.py
@@ -1,3 +1,6 @@
+from typing import Callable
+
+
 def sizeof_fmt(num, suffix='B'):
     """Size to human readable format.
 
@@ -8,3 +11,14 @@ def sizeof_fmt(num, suffix='B'):
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
+def nested_map(function: Callable):
+    """Return function that applies function to nested lists."""
+    def mapper(item):
+        if isinstance(item, list):
+            return [mapper(x) for x in item]
+        else:
+            return function(item)
+
+    return mapper
