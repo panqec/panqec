703d79097d162a3c81728230b6f92f64c30edf39
ehua7365 <ehua7365@users.noreply.github.com>
2021-04-16 18:06:33 +1000

    [Add] test creating a pauli error model for 5qc

--- tests/qecsim/test_fivequbit.py
+++ tests/qecsim/test_fivequbit.py
@@ -4,6 +4,7 @@ Test five qubit code very simple run.
 
 import pytest
 import numpy as np
+from bn3d.models import PauliErrorModel
 from qecsim.models.basic import FiveQubitCode
 from qecsim.paulitools import pauli_to_bsf
 
@@ -35,3 +36,13 @@ def test_logicals(five_qubit_code):
         pauli_to_bsf(pauli_string)
         for pauli_string in expected_logical_zs
     ]))
+
+
+def test_noise_model(five_qubit_code):
+    code = five_qubit_code
+    error_model = PauliErrorModel((0.5, 0.1, 0.4))
+    probability = 0.3
+    np.random.seed(0)
+    assert hasattr(error_model, 'generate')
+    error = error_model.generate(code, probability, rng=np.random)
+    assert error.shape == (10, )
