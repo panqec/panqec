2d26cd200ccf96d5b35531c526db149597436d03
ehua7365 <ehua7365@users.noreply.github.com>
2021-04-18 09:38:43 +1000

    [Add] test for running 5-qubit code simulation

--- tests/qecsim/test_fivequbit.py
+++ tests/qecsim/test_fivequbit.py
@@ -2,6 +2,7 @@
 Test five qubit code very simple run.
 """
 
+import json
 import pytest
 import numpy as np
 from bn3d.models import PauliErrorModel
@@ -9,6 +10,7 @@ import qecsim.paulitools as pt
 from qecsim.models.basic import FiveQubitCode
 from qecsim.models.generic import NaiveDecoder
 from qecsim.paulitools import pauli_to_bsf, bsf_to_pauli
+from qecsim.app import run
 
 
 @pytest.fixture
@@ -18,9 +20,14 @@ def five_qubit_code():
 
 
 @pytest.fixture
-def pauli_noise_model(five_qubit_code):
+def pauli_noise_model():
     error_model = PauliErrorModel((0.5, 0.1, 0.4))
     return error_model
+
+
+@pytest.fixture
+def naive_decoder():
+    return NaiveDecoder()
 
 
 def test_stabilizers(five_qubit_code):
@@ -56,10 +63,10 @@ def test_generate_noise(five_qubit_code, pauli_noise_model):
     assert error.shape == (10, )
 
 
-def test_decoder(five_qubit_code, pauli_noise_model):
+def test_decoder(five_qubit_code, pauli_noise_model, naive_decoder):
     code = five_qubit_code
     error_model = pauli_noise_model
-    decoder = NaiveDecoder()
+    decoder = naive_decoder
     assert hasattr(decoder, 'decode')
 
     # Want deterministic results.
@@ -90,3 +97,41 @@ def test_decoder(five_qubit_code, pauli_noise_model):
     total_error = (correction + error) % 2
     assert np.all(total_error == 0)
     assert bsf_to_pauli(total_error) == 'IIIII'
+
+
+def test_run(five_qubit_code, pauli_noise_model, naive_decoder):
+    code = five_qubit_code
+    error_model = pauli_noise_model
+    decoder = naive_decoder
+    error_probability = 0.4
+    results = run(
+        code, error_model, decoder, error_probability, max_runs=None,
+        max_failures=None, random_seed=0
+    )
+    assert type(results) is dict
+    assert sorted(results.keys()) == [
+        'code',
+        'custom_totals',
+        'decoder',
+        'error_model',
+        'error_probability',
+        'error_weight_pvar',
+        'error_weight_total',
+        'logical_failure_rate',
+        'measurement_error_probability',
+        'n_fail',
+        'n_k_d',
+        'n_logical_commutations',
+        'n_run',
+        'n_success',
+        'physical_error_rate',
+        'time_steps',
+        'wall_time',
+    ]
+    assert results['n_k_d'] == (5, 1, 3)
+    assert len(results['n_logical_commutations']) == 2
+    assert results['code'] == '5-qubit'
+
+    json_string = json.dumps(results)
+    parsed_results = json.loads(json_string)
+    assert results.keys() == parsed_results.keys()
