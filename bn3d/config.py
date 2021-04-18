c821ef4143e3a4e995e5f1199e1f60bcf03f5cae
ehua7365 <ehua7365@users.noreply.github.com>
2021-04-18 11:47:58 +1000

    [Add] dark theme config

--- bn3d/config.py
+++ bn3d/config.py
@@ -10,12 +10,21 @@ import os
 # Load the .env file into environmental variables.
 load_dotenv()
 
-# Load the output directory from environmental variables.
-BN3D_DIR = os.getenv('BN3D_DIR')
+BN3D_DARK_THEME = False
+if os.getenv('BN3D_DARK_THEME'):
+    BN3D_DARK_THEME = bool(os.getenv('BN3D_DARK_THEME'))
 
 # Fallback is to use temp dir inside repo if BN3D_DIR is not available.
-if BN3D_DIR is None or not os.path.exists(BN3D_DIR):
-    BN3D_DIR = os.path.join(
-        os.path.abspath(os.path.dirname(os.path.dirname(__file__))),
-        'temp'
-    )
+BN3D_DIR = os.path.join(
+    os.path.abspath(os.path.dirname(os.path.dirname(__file__))),
+    'temp'
+)
+
+# Load the output directory from environmental variables.
+if os.getenv('BN3D_DIR') is not None:
+    BN3D_DIR = os.path.abspath(str(os.getenv('BN3D_DIR')))
+    if not os.path.isdir(BN3D_DIR):
+        raise FileNotFoundError(
+            f'BN3D_DIR={BN3D_DIR} is not a valid directory. '
+            'Check .env configuration.'
+        )
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,10 @@
 import os
-from bn3d.config import BN3D_DIR
+from bn3d.config import BN3D_DIR, BN3D_DARK_THEME
 
 
 def test_output_dir_exists():
     assert os.path.exists(BN3D_DIR)
+
+
+def test_dark_theme_known():
+    assert type(BN3D_DARK_THEME) is bool
