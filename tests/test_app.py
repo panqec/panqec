57ce11257f2d0af7bf6cf5fe7280771eb9f57d7c
ehua7365 <ehua7365@users.noreply.github.com>
2021-05-01 18:04:36 +1000

    [Add] test for run_once

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 import os
 import pytest
-from bn3d.app import read_input_json
+import numpy as np
+from qecsim.models.basic import FiveQubitCode
+from qecsim.models.generic import NaiveDecoder
+from bn3d.noise import PauliErrorModel
+from bn3d.app import read_input_json, run_once
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
@@ -17,3 +21,18 @@ def test_read_json_input(file_name, expected_runs):
     batch_simulation = read_input_json(single_input_json)
     assert batch_simulation is not None
     assert len(batch_simulation._simulations) == expected_runs
+
+
+def test_run_once():
+    code = FiveQubitCode()
+    decoder = NaiveDecoder()
+    direction = np.ones(3)/3
+    error_model = PauliErrorModel(*direction)
+    probability = 0.1
+    results = run_once(
+        code, error_model, decoder, error_probability=probability
+    )
+    required_fields = [
+        'error', 'syndrome', 'correction', 'effective_error', 'success'
+    ]
+    assert all(field in results for field in required_fields)
