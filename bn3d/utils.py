40a56475fa738f607afb589923d51a516f7c254e
ehua7365 <ehua7365@users.noreply.github.com>
2021-05-03 05:49:37 +1000

    [Change] improved numpy json encoder

--- bn3d/utils.py
+++ bn3d/utils.py
@@ -39,7 +39,12 @@ def identity(x):
 
 
 class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
     def default(self, obj):
-        if isinstance(obj, np.ndarray):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
