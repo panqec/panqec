9b9cb6be0ee8c803207c7413f208447c32441845
Arthur Pesah <arthur.pesah@gmail.com>
2021-08-01 21:16:13 +0200

    Add regular SweepMatch decoder to GUI

--- gui3d.py
+++ gui3d.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from flask import Flask, send_from_directory, request, json
 from bn3d.bp_os_decoder import bp_osd_decoder
-from bn3d.tc3d import ToricCode3D
+from bn3d.tc3d import ToricCode3D, SweepMatchDecoder
 from bn3d.bp_os_decoder import BeliefPropagationOSDDecoder
 from bn3d.noise import PauliErrorModel
 from bn3d.deform import DeformedPauliErrorModel, DeformedSweepMatchDecoder
@@ -69,7 +69,10 @@ def send_correction():
                                               max_bp_iter=max_bp_iter,
                                               deformed=deformed)
     else:
-        decoder = DeformedSweepMatchDecoder(error_model, p)
+        if deformed:
+            decoder = DeformedSweepMatchDecoder(error_model, p)
+        else:
+            decoder = SweepMatchDecoder()
 
     correction = decoder.decode(code, syndrome)
     
