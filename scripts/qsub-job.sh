c56b27624209c567862379aa17dc86dfa44a3b23
Arthur Pesah <arthur.pesah@gmail.com>
2021-08-10 17:19:12 +0200

    [Change] Remove -t option from script and other simplifications

--- scripts/qsub-job.sh
+++ scripts/qsub-job.sh
@@ -11,19 +11,12 @@
 # Request 15 gigabyte of TMPDIR space (default is 10 GB - remove if cluster is diskless)
 #$ -l tmpfs=1G
 
-# Set up the job array.  In this instance we have requested 1000 tasks
-# numbered 1 to 1000.
-#$ -t 1-`ls results/input/ | wc -l`
-
-
 # Set the name of the job.
 #$ -N TestJob
 
 # Automate transfer of output to Scratch from $TMPDIR.
 #Local2Scratch
 
-cd $TMPDIR
-
 conda activate bn3d
 filename="`ls` | awk '{print $3}'"
-bn3d run --file /home/ucapacp/bn3d/results/input/$filename --trials 20 -o /home/ucapacp/Scratch/output
+bn3d run --file /home/ucapacp/bn3d/results/input/$filename --trials 20 -o $TMPDIR
