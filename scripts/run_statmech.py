79a5de1259b95a39801fd49aede19ebb0e54d77e
ehua7365 <ehua7365@users.noreply.github.com>
2021-10-04 22:04:59 -0400

    [Add] fixed slurm scripts

--- scripts/run_statmech.py
+++ scripts/run_statmech.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from bn3d.statmech.rbim2d import Rbim2DIidDisorder
 from bn3d.statmech.controllers import SimpleController
 import psutil
@@ -9,19 +9,19 @@ from itertools import product
 
 def generate_inputs(n_disorder):
     entries = []
-    temperatures = np.arange(1, 2, 0.25).round(6).tolist()
+    temperatures = np.arange(0.6, 2, 0.2).round(6).tolist()
     spin_model_params_list = [
         {
             'L_x': int(L),
             'L_y': int(L),
         }
-        for L in [8, 10, 12]
+        for L in [8, 10, 12, 16]
     ]
     disorder_params_list = [
         {
             'p': float(p),
         }
-        for p in [0.05]
+        for p in [0.02, 0.05]
     ]
     for spin_model_params, disorder_params in product(
         spin_model_params_list, disorder_params_list
@@ -49,8 +49,8 @@ def main():
     print(psutil.cpu_percent(percpu=True))
 
     data_dir = sys.argv[1]
-    n_disorder = 3
-    max_tau = 6
+    n_disorder = 100
+    max_tau = 10
 
     controller = SimpleController(data_dir)
     controller.init_models(generate_inputs(n_disorder))
