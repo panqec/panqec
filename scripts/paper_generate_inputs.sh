03fff5f42a52889797ec03b7ea43a6b9bee1fbf5
ehua7365 <ehua7365@users.noreply.github.com>
2021-10-21 18:12:25 -0400

    [Change] inputs for subthreshold scaling

--- scripts/paper_generate_inputs.sh
+++ scripts/paper_generate_inputs.sh
@@ -4,22 +4,23 @@ sbatch_dir=temp/paper/sbatch
 mkdir -p "$sbatch_dir"
 
 
-function setup_dir () {
-	local name=$1 rotation=$2 decoder=$3 deformation=$4 bias=$5
+setup_dir() {
+    local name=$1 rotation=$2 decoder=$3 deformation=$4 bias=$5 eta=$6 \
+            prob=$7 trials=$8
     echo "$name $rotation $decoder $deformation $bias"
     if [ $rotation = rotated ]; then
         boundary=planar
     else
         boundary=toric
     fi
-	input_dir="$paper_dir/$name/inputs"
-	mkdir -p $input_dir
-	bn3d generate-input -i $input_dir \
-            -l $rotation -b $boundary -d $deformation -r equal \
-            -s 2,4,6,8 --decoder $decoder --bias $bias
+    input_dir="$paper_dir/$name/inputs"
+    mkdir -p $input_dir
+    bn3d generate-input -i $input_dir \
+        -l $rotation -b $boundary -d $deformation -r equal \
+        -s 2,4,6,8 --decoder $decoder --bias $bias \
+        --eta $eta --prob $prob
 
     walltime='0-03:00'
-    trials=1000
     narray=6
     queue=defq
     sbatch_file="$sbatch_dir/$name.sbatch"
@@ -31,20 +32,64 @@ function setup_dir () {
     sed -i -- "s/\${QUEUE}/$queue/g" "$sbatch_file"
 }
 
-setup_dir rot_bposd_undef_xbias rotated BeliefPropagationOSDDecoder none X
-setup_dir rot_bposd_undef_zbias rotated BeliefPropagationOSDDecoder none Z
-setup_dir rot_bposd_xzzx_xbias rotated BeliefPropagationOSDDecoder xzzx X
-setup_dir rot_bposd_xzzx_zbias rotated BeliefPropagationOSDDecoder xzzx Z
-setup_dir rot_sweepmatch_undef_xbias rotated RotatedSweepMatchDecoder none X
-setup_dir rot_sweepmatch_undef_zbias rotated RotatedSweepMatchDecoder none Z
-setup_dir rot_sweepmatch_xy_xbias rotated RotatedSweepMatchDecoder xy X
-setup_dir rot_sweepmatch_xy_zbias rotated RotatedSweepMatchDecoder xy Z
+setup_dir sts_rot_bposd_undef_zbias rotated BeliefPropagationOSDDecoder none Z \
+        "1,10,100,1000,inf" "0.05" 100000
+setup_dir sts_rot_bposd_xzzx_zbias rotated BeliefPropagationOSDDecoder xzzx Z \
+        "1,10,100,1000,inf" "0.05" 100000
 
-setup_dir unrot_bposd_undef_xbias kitaev BeliefPropagationOSDDecoder none X
-setup_dir unrot_bposd_undef_zbias kitaev BeliefPropagationOSDDecoder none Z
-setup_dir unrot_bposd_xzzx_xbias kitaev BeliefPropagationOSDDecoder xzzx X
-setup_dir unrot_bposd_xzzx_zbias kitaev BeliefPropagationOSDDecoder xzzx Z
-setup_dir unrot_sweepmatch_undef_xbias kitaev SweepMatchDecoder none X
-setup_dir unrot_sweepmatch_undef_zbias kitaev SweepMatchDecoder none Z
-setup_dir unrot_sweepmatch_xy_xbias kitaev SweepMatchDecoder xy X
-setup_dir unrot_sweepmatch_xy_zbias kitaev SweepMatchDecoder xy Z
+setup_dir sts_rot_sweepmatch_undef_zbias rotated RotatedSweepMatchDecoder none Z \
+        "1,10,100,1000,inf" "0.05" 100000
+setup_dir sts_rot_sweepmatch_xy_zbias rotated RotatedSweepMatchDecoder xy Z \
+        "1,10,100,1000,inf" "0.05" 100000
+
+setup_dir rot_bposd_undef_zbias rotated BeliefPropagationOSDDecoder none Z \
+        "0.5,1,3" "0:0.18:0.01" 10000
+setup_dir rot_bposd_undef_zbias rotated BeliefPropagationOSDDecoder none Z \
+        "10,30,100,inf" "0.18:0.27:0.01" 10000
+
+setup_dir rot_bposd_xzzx_zbias rotated BeliefPropagationOSDDecoder xzzx Z \
+        "0.5,1,3" "0:0.18:0.01" 10000
+setup_dir rot_bposd_xzzx_zbias rotated BeliefPropagationOSDDecoder xzzx Z \
+        "10,30,100,inf" "0:0.55:0.01" 10000
+
+setup_dir rot_sweepmatch_undef_zbias rotated RotatedSweepMatchDecoder none Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01" 10000
+
+setup_dir rot_sweepmatch_xy_zbias rotated RotatedSweepMatchDecoder xy Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01" 10000
+
+: '
+setup_dir rot_bposd_undef_xbias rotated BeliefPropagationOSDDecoder none X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_bposd_undef_zbias rotated BeliefPropagationOSDDecoder none Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_bposd_xzzx_xbias rotated BeliefPropagationOSDDecoder xzzx X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_bposd_xzzx_zbias rotated BeliefPropagationOSDDecoder xzzx Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_sweepmatch_undef_xbias rotated RotatedSweepMatchDecoder none X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_sweepmatch_undef_zbias rotated RotatedSweepMatchDecoder none Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_sweepmatch_xy_xbias rotated RotatedSweepMatchDecoder xy X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir rot_sweepmatch_xy_zbias rotated RotatedSweepMatchDecoder xy Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+
+setup_dir unrot_bposd_undef_xbias kitaev BeliefPropagationOSDDecoder none X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_bposd_undef_zbias kitaev BeliefPropagationOSDDecoder none Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_bposd_xzzx_xbias kitaev BeliefPropagationOSDDecoder xzzx X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_bposd_xzzx_zbias kitaev BeliefPropagationOSDDecoder xzzx Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_sweepmatch_undef_xbias kitaev SweepMatchDecoder none X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_sweepmatch_undef_zbias kitaev SweepMatchDecoder none Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_sweepmatch_xy_xbias kitaev SweepMatchDecoder xy X \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+setup_dir unrot_sweepmatch_xy_zbias kitaev SweepMatchDecoder xy Z \
+        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
+'
