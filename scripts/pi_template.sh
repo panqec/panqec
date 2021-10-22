5d03173c200287f045c7923a23d7332d2abf7905
ehua7365 <ehua7365@users.noreply.github.com>
2021-10-22 14:25:56 -0400

    [Add] much improved sbatch template

--- scripts/pi_template.sh
+++ scripts/pi_template.sh
@@ -11,13 +11,16 @@ set -euxo pipefail
 
 # Variables to change.
 trials=${TRIALS}
-data_dir=temp/paper/${NAME}
-input_dir="$data_dir/inputs"
-output_dir="$data_dir/results"
-log_dir="$data_dir/logs"
-bash_command="bn3d run -f {} -o $output_dir -t $trials"
+data_dir=${DATADIR}
+n_split=${SPLIT}
 
-# Print out the current working directory.
+# The input directory.
+input_dir="$data_dir/inputs"
+
+# The bash command to parallelize.
+bash_command="bn3d run -f $input_dir/{1}.json -o $data_dir/{2} -t {3}"
+
+# Print out the current working directory so we know where we are.
 pwd
 
 # Load python and activate the python virtual environment.
@@ -25,9 +28,9 @@ module purge
 module load python/3.8
 source venv/bin/activate
 
-# Create the subdirectories
-mkdir -p output_dir
-mkdir -p log_dir
+# Create the subdirectory for storing logs.
+log_dir="$data_dir/logs"
+mkdir -p $log_dir
 
 # Print out the environmental variables and the time.
 printenv
@@ -38,19 +41,48 @@ i_task=$SLURM_ARRAY_TASK_ID
 # Run a CPU and RAM usage logging script in the background.
 python scripts/monitor.py "$log_dir/usage_${SLURM_JOB_ID}_${i_task}.txt" &
 
-# Function that prints out filtered functions.
-function filter_files() {
+# Function that prints out tab-separated values with columns input name,
+# results directory name and number of trials to do for that split.
+function filter_input {
     counter=0
+    
+    # Iterate through files in the input directory.
     for filename in $input_dir/*.json; do
-        if [[ $(( counter % n_tasks + 1 )) == $i_task ]]; then
-            echo $filename
+
+        # Just the file name without directory and extension.
+        input_name=$(basename -s .json $filename)
+
+        # Each array job gets assigned some input files.
+        if [[ $(( counter % n_tasks + 1 )) -eq $i_task ]]; then
+
+            # Name the results directory 'results' if no splitting.
+            if [ $n_split -eq 1 ]; then
+                results_dir=results
+                split_trials=$trials
+                echo -e "$input_name\t$results_dir\t$split_trials"
+
+            # Split the results over directories results_1, results_2, etc.
+            else
+                for i_split in $(seq 1 $n_split); do
+                    results_dir="results_$i_split"
+                    mkdir -p $data_dir/$results_dir
+                    split_trials=$(( (trials - trials % n_split)/n_split ))
+                    if [ $i_split -eq $n_split ]; then
+                        split_trials=$(( split_trials + trials % n_split ))
+                    fi
+                    echo -e "$input_name\t$results_dir\t$split_trials"
+                done
+            fi
         fi
         counter=$(( counter + 1 ))
     done
 }
 
+# Write filtered input into GNU parallel to text file in logs.
+filter_input  >> $log_dir/filter_${SLURM_JOB_ID}_${i_task}.txt
+
 # Run in parallel.
-filter_files | parallel --results $log_dir $bash_command :::
+filter_input | parallel --colsep '\t' --results $log_dir "$bash_command"
 
 # Print out the date when done.
 date
