38db25d154c045b4376c7797aec038f643612e45
ehua7365 <ehua7365@users.noreply.github.com>
2021-11-15 07:53:52 -0500

    [Change] inputs to rerun with more sweeps

--- scripts/paper_generate_inputs.sh
+++ scripts/paper_generate_inputs.sh
@@ -102,6 +102,7 @@ bn3d pi-sbatch --data_dir "$paper_dir/$name" --n_array 6 --queue $queue \
     --wall_time "$wall_time" --trials 10000 --split 10 $sbatch_dir/$name.sbatch
 '
 
+# Rough runs using SweepMatch decoder on rotated code.
 name=rot_sweepmatch_undef_zbias
 bn3d generate-input -i "$paper_dir/$name/inputs" \
     --lattice rotated --boundary planar --deformation none --ratio "$ratio" \
@@ -118,6 +119,8 @@ bn3d generate-input -i "$paper_dir/$name/inputs" \
 bn3d pi-sbatch --data_dir "$paper_dir/$name" --n_array 6 --queue $queue \
     --wall_time "$wall_time" --trials 1000 --split 10 $sbatch_dir/$name.sbatch
 
+: '
+# Rough runs using SweepMatch decoder on toric code
 name=unrot_sweepmatch_undef_zbias
 bn3d generate-input -i "$paper_dir/$name/inputs" \
     --lattice kitaev --boundary toric --deformation none  --ratio "$ratio" \
@@ -134,7 +137,6 @@ bn3d generate-input -i "$paper_dir/$name/inputs" \
 bn3d pi-sbatch --data_dir "$paper_dir/$name" --n_array 6 --queue $queue \
     --wall_time "$wall_time" --trials 1000 --split 10 $sbatch_dir/$name.sbatch
 
-: '
 # Detailed run infinite Z bias
 name=det_rot_bposd_xzzx_zbias  
 bn3d generate-input -i "$paper_dir/$name/inputs" \
@@ -143,40 +145,4 @@ bn3d generate-input -i "$paper_dir/$name/inputs" \
     --eta "inf" --prob "0.32:0.38:0.005"
 bn3d pi-sbatch --data_dir "$paper_dir/$name" --n_array 12 --queue $queue \
     --wall_time "$wall_time" --trials 100000 --split 25 $sbatch_dir/$name.sbatch
-'
-
-: '
-setup_dir rot_bposd_undef_xbias rotated BeliefPropagationOSDDecoder none X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_bposd_undef_zbias rotated BeliefPropagationOSDDecoder none Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_bposd_xzzx_xbias rotated BeliefPropagationOSDDecoder xzzx X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_bposd_xzzx_zbias rotated BeliefPropagationOSDDecoder xzzx Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_sweepmatch_undef_xbias rotated RotatedSweepMatchDecoder none X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_sweepmatch_undef_zbias rotated RotatedSweepMatchDecoder none Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_sweepmatch_xy_xbias rotated RotatedSweepMatchDecoder xy X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir rot_sweepmatch_xy_zbias rotated RotatedSweepMatchDecoder xy Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-
-setup_dir unrot_bposd_undef_xbias kitaev BeliefPropagationOSDDecoder none X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_bposd_undef_zbias kitaev BeliefPropagationOSDDecoder none Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_bposd_xzzx_xbias kitaev BeliefPropagationOSDDecoder xzzx X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_bposd_xzzx_zbias kitaev BeliefPropagationOSDDecoder xzzx Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_sweepmatch_undef_xbias kitaev SweepMatchDecoder none X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_sweepmatch_undef_zbias kitaev SweepMatchDecoder none Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_sweepmatch_xy_xbias kitaev SweepMatchDecoder xy X \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
-setup_dir unrot_sweepmatch_xy_zbias kitaev SweepMatchDecoder xy Z \
-        "0.5,1,3,10,30,100,inf" "0:0.55:0.01"
 '
