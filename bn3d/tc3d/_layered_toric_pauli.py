5c266100823dfb024f7bde30acddf9a99b89afe0
ehua7365 <ehua7365@users.noreply.github.com>
2021-11-26 14:17:54 -0500

    [Add] layered toric pauli vertex method
    Enforcing periodic boundary conditions in x and y but m boundaries for
    z.

--- bn3d/tc3d/_layered_toric_pauli.py
+++ bn3d/tc3d/_layered_toric_pauli.py
@@ -5,7 +5,30 @@ from ._rotated_planar_3d_pauli import RotatedPlanar3DPauli
 class LayeredToricPauli(RotatedPlanar3DPauli):
 
     def vertex(self, operator: str, location: Tuple[int, int, int]):
-        pass
+        L_x, L_y, L_z = self.code.size
+        x, y, z = location
+        neighbours = []
+
+        # Edges along unrotated x.
+        neighbours += [
+            ((x + 1) % (2*L_x), y - 1, z),
+            (x - 1, (y + 1) % (2*L_y), z)
+        ]
+
+        # Edges along unrotated y.
+        neighbours += [
+            ((x + 1) % (2*L_x), (y + 1) % (2*L_y), z),
+            (x - 1, y - 1, z)
+        ]
+
+        # Vertical edges along unrotated z, if they are still on the lattice.
+        if z + 1 <= 2*L_z + 1:
+            neighbours.append((x, y, z + 1))
+        if z - 1 >= 1:
+            neighbours.append((x, y, z - 1))
+
+        for edge in neighbours:
+            self.site(operator, edge)
 
     def face(self, operator: str, location: Tuple[int, int, int]):
         pass
