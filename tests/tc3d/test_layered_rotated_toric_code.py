53f0ff92db1a54f610b2930834bd9343dbc7cb25
ehua7365 <ehua7365@users.noreply.github.com>
2021-11-26 09:27:12 -0500

    [Add] tests for coordinates of small lattices

--- tests/tc3d/test_layered_rotated_toric_code.py
+++ tests/tc3d/test_layered_rotated_toric_code.py
@@ -1,18 +1,140 @@
+from typing import Tuple, List
+from abc import ABCMeta, abstractmethod
 import pytest
 from bn3d.tc3d import LayeredRotatedToricCode
+
 from .indexed_code_test import IndexedCodeTest
 
 
-class TestLayeredRotatedToricCode(IndexedCodeTest):
+class IndexedCodeTestWithCoordinates(IndexedCodeTest, metaclass=ABCMeta):
 
-    L_x = 3
-    L_y = 4
-    L_z = 3
+    @property
+    @abstractmethod
+    def size(self) -> Tuple[int, int, int]:
+        """Plane size x"""
+
+    @property
+    @abstractmethod
+    def expected_plane_edges_xy(self) -> List[Tuple[int, int]]:
+        """Expected xy coordinates of xy plane edges in unrotated lattice."""
+
+    @property
+    @abstractmethod
+    def expected_plane_faces_xy(self) -> List[Tuple[int, int]]:
+        """Expected xy coordinates of xy plane faces in unrotated lattice."""
+
+    @property
+    @abstractmethod
+    def expected_plane_vertices_xy(self) -> List[Tuple[int, int]]:
+        """Expected xy coordinates of xy plane vertices in unrot lattice."""
+
+    @property
+    @abstractmethod
+    def expected_plane_z(self) -> List[int]:
+        """Expected z coordinates of horizontal planes."""
+
+    @property
+    @abstractmethod
+    def expected_vertical_z(self) -> List[int]:
+        """Expected z coordinates of vertical edges and faces."""
 
     @pytest.fixture
     def code(self):
-        new_code = LayeredRotatedToricCode(self.L_x, self.L_y, self.L_z)
+        new_code = LayeredRotatedToricCode(*self.size)
         return new_code
+
+    def test_qubit_indices(self, code):
+        locations = set(code.qubit_index.keys())
+        expected_locations = set([
+            (x, y, z)
+            for x, y in self.expected_plane_edges_xy
+            for z in self.expected_plane_z
+        ] + [
+            (x, y, z)
+            for x, y in self.expected_plane_vertices_xy
+            for z in self.expected_vertical_z
+        ])
+        assert locations == expected_locations
+
+    def test_vertex_indices(self, code):
+        locations = set(code.vertex_index.keys())
+        expected_locations = set([
+            (x, y, z)
+            for x, y in self.expected_plane_vertices_xy
+            for z in self.expected_plane_z
+        ])
+        assert locations == expected_locations
+
+    def test_face_indices(self, code):
+        locations = set(code.face_index.keys())
+        expected_locations = set([
+            (x, y, z)
+            for x, y in self.expected_plane_edges_xy
+            for z in self.expected_vertical_z
+        ] + [
+            (x, y, z)
+            for x, y in self.expected_plane_faces_xy
+            for z in self.expected_plane_z
+        ])
+        assert locations == expected_locations
+
+
+class TestLayeredRotatedToricCode2x2x1(IndexedCodeTestWithCoordinates):
+    size = (2, 2, 1)
+    expected_plane_edges_xy = [
+        (1, 1), (3, 1),
+        (1, 3), (3, 3),
+    ]
+    expected_plane_faces_xy = [
+        (2, 2), (4, 4)
+    ]
+    expected_plane_vertices_xy = [
+        (4, 2), (2, 4)
+    ]
+    expected_plane_z = [1, 3]
+    expected_vertical_z = [2]
+
+
+class TestLayeredRotatedToricCode4x3x3(IndexedCodeTestWithCoordinates):
+    size = (4, 3, 3)
+    expected_plane_edges_xy = [
+        (1, 1), (3, 1), (5, 1), (7, 1),
+        (1, 3), (3, 3), (5, 3), (7, 3),
+        (1, 5), (3, 5), (5, 5), (7, 5),
+    ]
+    expected_plane_faces_xy = [
+        (2, 2), (6, 2),
+        (4, 4), (8, 4),
+        (2, 6), (6, 6),
+    ]
+    expected_plane_vertices_xy = [
+        (4, 2), (8, 2),
+        (2, 4), (6, 4),
+        (4, 6), (8, 6),
+    ]
+    expected_plane_z = [1, 3, 5, 7]
+    expected_vertical_z = [2, 4, 6]
+
+
+class TestLayeredRotatedToricCode3x4x3(IndexedCodeTestWithCoordinates):
+    size = (3, 4, 3)
+    expected_plane_edges_xy = [
+        (1, 1), (1, 3), (1, 5), (1, 7),
+        (3, 1), (3, 3), (3, 5), (3, 7),
+        (5, 1), (5, 3), (5, 5), (5, 7),
+    ]
+    expected_plane_faces_xy = [
+        (2, 2), (2, 6),
+        (4, 4), (4, 8),
+        (6, 2), (6, 6),
+    ]
+    expected_plane_vertices_xy = [
+        (2, 4), (2, 8),
+        (4, 2), (4, 6),
+        (6, 4), (6, 8),
+    ]
+    expected_plane_z = [1, 3, 5, 7]
+    expected_vertical_z = [2, 4, 6]
 
 
 class TestLayeredRotatedToricPauli:
