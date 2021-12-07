32f7f0296b96d24fc008493c3af41de95c8a7eab
ehua7365 <ehua7365@users.noreply.github.com>
2021-12-07 11:49:05 -0500

    [Change] allow multiple folders merge zip

--- scripts/merge_zip.sh
+++ scripts/merge_zip.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-name=$1
-bn3d merge-dirs -o temp/paper/$name/results
-cd temp/paper/$name/
-zip -r $name.zip inputs results
-cd -
-mv temp/paper/$name/$name.zip temp/paper/share
+for name in "$@"; do
+    bn3d merge-dirs -o temp/paper/$name/results
+    cd temp/paper/$name/
+    zip -r $name.zip inputs results
+    cd -
+    mv temp/paper/$name/$name.zip temp/paper/share
+done
