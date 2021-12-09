9c6d5af4682b81dad4302c5f6affecf7a1008ec2
ehua7365 <ehua7365@users.noreply.github.com>
2021-12-09 15:51:48 -0500

    [Change] delete inputs folder before generating

--- scripts/cc_generate_inputs.sh
+++ scripts/cc_generate_inputs.sh
@@ -8,6 +8,7 @@ memory="16GB"
 
 # Rough runs using BPOSD decoder on toric code
 name=unrot_bposd_xzzx_zbias
+rm -rf $paper_dir/$name/inputs
 bn3d generate-input -i "$paper_dir/$name/inputs" \
     --lattice kitaev --boundary toric --deformation xzzx --ratio equal  \
     --sizes "5,9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
@@ -16,6 +17,7 @@ bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 100 \
     --wall_time "$wall_time" --trials 10000 --split 10 $sbatch_dir/$name.sbatch
 
 name=unrot_bposd_undef_zbias
+rm -rf $paper_dir/$name/inputs
 bn3d generate-input -i "$paper_dir/$name/inputs" \
     --lattice kitaev --boundary toric --deformation none --ratio equal \
     --sizes "5,9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
