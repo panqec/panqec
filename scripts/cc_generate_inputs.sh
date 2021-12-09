c417a549e476d4068edb63b399bde4d5ea02b47e
ehua7365 <ehua7365@users.noreply.github.com>
2021-12-09 16:45:40 -0500

    [Change] split over less jobs

--- scripts/cc_generate_inputs.sh
+++ scripts/cc_generate_inputs.sh
@@ -7,7 +7,7 @@ wall_time="23:50:00"
 memory="124GB"
 
 # Rough runs using BPOSD decoder on toric code
-for repeat in $(seq 1 10); do
+for repeat in $(seq 1 6); do
     name=unrot_bposd_xzzx_zbias_$repeat
     rm -rf $paper_dir/$name/inputs
     rm -rf $paper_dir/$name/logs
@@ -16,7 +16,7 @@ for repeat in $(seq 1 10); do
         --sizes "5,9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
         --eta "30,100,inf" --prob "0:0.5:0.02"
     bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 78 \
-        --wall_time "$wall_time" --trials 1000 --split 3 $sbatch_dir/$name.sbatch
+        --wall_time "$wall_time" --trials 1667 --split 3 $sbatch_dir/$name.sbatch
 
     name=unrot_bposd_undef_zbias_$repeat
     rm -rf $paper_dir/$name/inputs
@@ -26,5 +26,5 @@ for repeat in $(seq 1 10); do
         --sizes "5,9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
         --eta "30,100,inf" --prob "0:0.5:0.02"
     bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 78 \
-        --wall_time "$wall_time" --trials 1000 --split 3 $sbatch_dir/$name.sbatch
+        --wall_time "$wall_time" --trials 1667 --split 3 $sbatch_dir/$name.sbatch
 done
