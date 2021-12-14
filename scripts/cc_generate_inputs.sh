d37c2ceac2970734626d1b9973e51ec32f78bd87
ehua7365 <ehua7365@users.noreply.github.com>
2021-12-14 11:48:19 -0500

    [Change] input parameters fixed memory

--- scripts/cc_generate_inputs.sh
+++ scripts/cc_generate_inputs.sh
@@ -3,7 +3,7 @@ mkdir -p "$paper_dir"
 sbatch_dir=temp/paper/sbatch
 mkdir -p "$sbatch_dir"
 
-wall_time="23:50:00"
+wall_time="23:59:00"
 memory="124GB"
 
 # Rough runs using BPOSD decoder on toric code
@@ -13,9 +13,9 @@ for repeat in $(seq 1 6); do
     rm -rf $paper_dir/$name/logs
     bn3d generate-input -i "$paper_dir/$name/inputs" \
         --lattice kitaev --boundary toric --deformation xzzx --ratio equal  \
-        --sizes "5,9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
-        --eta "30,100,inf" --prob "0:0.5:0.02"
-    bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 78 \
+        --sizes "9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
+        --eta "30,100" --prob "0:0.5:0.02"
+    bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 50 --memory $memory \
         --wall_time "$wall_time" --trials 1667 --split 3 $sbatch_dir/$name.sbatch
 
     name=unrot_bposd_undef_zbias_$repeat
@@ -23,8 +23,8 @@ for repeat in $(seq 1 6); do
     rm -rf $paper_dir/$name/logs
     bn3d generate-input -i "$paper_dir/$name/inputs" \
         --lattice kitaev --boundary toric --deformation none --ratio equal \
-        --sizes "5,9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
-        --eta "30,100,inf" --prob "0:0.5:0.02"
-    bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 78 \
+        --sizes "9,13,17,21" --decoder BeliefPropagationOSDDecoder --bias Z \
+        --eta "30,100" --prob "0:0.5:0.02"
+    bn3d cc-sbatch --data_dir "$paper_dir/$name" --n_array 50 --memory $memory \
         --wall_time "$wall_time" --trials 1667 --split 3 $sbatch_dir/$name.sbatch
 done
