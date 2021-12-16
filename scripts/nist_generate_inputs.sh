1fd115ef4d0fa7314ef78fac2eb8f1452a309c7a
ehua7365 <ehua7365@users.noreply.github.com>
2021-12-15 22:28:59 -0500

    [Add] detailed ranges

--- scripts/nist_generate_inputs.sh
+++ scripts/nist_generate_inputs.sh
@@ -3,28 +3,72 @@ mkdir -p "$paper_dir"
 sbatch_dir=temp/paper/sbatch
 mkdir -p "$sbatch_dir"
 
-wall_time="0-23:50"
-memory="90GB"
+wall_time="4-15:00"
+memory="93GB"
 
-# name=rot_bposd_xzzx_zbias
-# rm -rf $paper_dir/$name/inputs
-# rm -rf $paper_dir/$name/logs
-# bn3d generate-input -i "$paper_dir/$name/inputs" \
-    # --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
-    # --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
-    # --eta "0.5,1,3,10,30,100,inf" --prob "0:0.5:0.02"
-# bn3d nist-sbatch --data_dir "$paper_dir/$name" --n_array 50 --memory "$memory" \
-    # --wall_time "$wall_time" --trials 2000 --split 2 $sbatch_dir/$name.sbatch
+name=det_rot_bposd_xzzx_zbias
+rm -rf $paper_dir/$name/inputs
+rm -rf $paper_dir/$name/logs
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "0.5" --prob "0.055:0.075:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "1" --prob "0.050:0.070:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "3" --prob "0.060:0.100:0.004"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "10" --prob "0.011:0.015:0.004"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "30" --prob "0.326:0.336:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation xzzx --ratio equal  \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "100" --prob "0.36:0.38:0.004"
+bn3d nist-sbatch --data_dir "$paper_dir/$name" --n_array 30 --memory "$memory" \
+    --wall_time "$wall_time" --trials 10000 --split 2 $sbatch_dir/$name.sbatch
 
-# name=rot_bposd_undef_zbias
-# rm -rf $paper_dir/$name/inputs
-# rm -rf $paper_dir/$name/logs
-# bn3d generate-input -i "$paper_dir/$name/inputs" \
-    # --lattice rotated --boundary planar --deformation none --ratio equal \
-    # --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
-    # --eta "0.5,1,3,10,30,100,inf" --prob "0:0.5:0.02"
-# bn3d nist-sbatch --data_dir "$paper_dir/$name" --n_array 50 --memory "$memory" \
-    # --wall_time "$wall_time" --trials 2000 --split 2 $sbatch_dir/$name.sbatch
+name=det_rot_bposd_undef_zbias
+rm -rf $paper_dir/$name/inputs
+rm -rf $paper_dir/$name/logs
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "0.5" --prob "0.055:0.075:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "0.5" --prob "0.066:0.086:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "3" --prob "0.120:0.140:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "10" --prob "0.218:0.238:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "30" --prob "0.208:0.228:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "100" --prob "0.206:0.226:0.002"
+bn3d generate-input -i "$paper_dir/$name/inputs" \
+    --lattice rotated --boundary planar --deformation none --ratio equal \
+    --sizes "4,6,8,10" --decoder BeliefPropagationOSDDecoder --bias Z \
+    --eta "inf" --prob "0.206:0.226:0.002"
+bn3d nist-sbatch --data_dir "$paper_dir/$name" --n_array 35 --memory "$memory" \
+    --wall_time "$wall_time" --trials 10000 --split 2 $sbatch_dir/$name.sbatch
 
 
 name=det_rhombic_bposd_undef_xbias
