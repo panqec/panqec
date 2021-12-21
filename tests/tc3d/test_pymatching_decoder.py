cdc75129aa230e00214eab432062edc8442d6597
Arthur Pesah <arthur.pesah@gmail.com>
2021-12-21 13:26:48 +0000

    Add pymatching test to sparse

--- tests/tc3d/test_pymatching_decoder.py
+++ tests/tc3d/test_pymatching_decoder.py
@@ -1,8 +1,7 @@
 import itertools
 import pytest
 import numpy as np
-from qecsim.paulitools import bsf_wt
-from bn3d.bpauli import bcommute
+from bn3d.bpauli import bcommute, bsf_wt
 from bn3d.models import ToricCode3D, Toric3DPauli
 from bn3d.decoders import Toric3DPymatchingDecoder
 
@@ -22,7 +21,7 @@ class TestToric3DPymatchingDecoder:
         assert decoder.decode is not None
 
     def test_decode_trivial_syndrome(self, decoder, code):
-        syndrome = np.zeros(shape=len(code.stabilizers), dtype=np.uint)
+        syndrome = np.zeros(shape=code.stabilizers.shape[0], dtype=np.uint)
         correction = decoder.decode(code, syndrome)
         assert correction.shape == 2*code.n_k_d[0]
         assert np.all(bcommute(code.stabilizers, correction) == 0)
@@ -39,6 +38,7 @@ class TestToric3DPymatchingDecoder:
 
         correction = decoder.decode(code, syndrome)
         total_error = (error.to_bsf() + correction) % 2
+
         assert np.all(bcommute(code.stabilizers, total_error) == 0)
 
     def test_decode_many_X_errors(self, decoder, code):
@@ -58,6 +58,7 @@ class TestToric3DPymatchingDecoder:
     def test_unable_to_decode_Z_error(self, decoder, code):
         error = Toric3DPauli(code)
         error.site('Z', (1, 0, 2))
+
         assert bsf_wt(error.to_bsf()) == 1
 
         syndrome = code.measure_syndrome(error)
