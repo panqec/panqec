f6331ff9546b8bc6e997e946dfd7cdfa874dadcf
ehua7365 <ehua7365@users.noreply.github.com>
2022-02-06 12:09:11 -0500

    [Change] fix more sparse tests

--- tests/tc3d/test_toric_code_3d.py
+++ tests/tc3d/test_toric_code_3d.py
@@ -15,10 +15,9 @@ class TestToricCode3D:
         new_code = ToricCode3D(L_x, L_y, L_z)
         return new_code
 
-    @pytest.mark.skip(reason='sparse')
     def test_cubic_code(self):
         code = ToricCode3D(5)
-        assert np.all(code.size == [5, 5, 5])
+        assert code.size == (5, 5, 5)
 
     def test_get_vertex_Z_stabilizers(self, code):
         n, k, d = code.n_k_d
