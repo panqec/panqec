a956200de7633dd52cc6611741d91ed362f40d6b
Arthur Pesah <arthur.pesah@gmail.com>
2022-02-11 20:24:45 +0000

    Correct bug in input size spec

--- scripts/qsub_generate_inputs.sh
+++ scripts/qsub_generate_inputs.sh
@@ -4,10 +4,10 @@ qsub_dir=temp/paper/qsub
 mkdir -p "$qsub_dir"
 mkdir -p temp/paper/share
 
-name=det_xcube_bposd_undef_xbias
+name=det_xcube_bposd_undef_zbias
 rm -rf $paper_dir/$name/inputs
 rm -rf $paper_dir/$name/logs
-sizes="4, 6"
+sizes="4,6"
 wall_time="14:00:00"
 memory="2G"
 
@@ -16,6 +16,7 @@ bn3d generate-input -i "$paper_dir/$name/inputs" \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias X \
     --eta "0.5" --prob "0.001:0.5:0.01"
+
 bn3d generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCodeCode --noise_class PauliErrorModel \
     --ratio equal \
@@ -23,4 +24,4 @@ bn3d generate-input -i "$paper_dir/$name/inputs" \
     --eta "100" --prob "0.001:0.5:0.01"
 
 bn3d generate-qsub --data_dir "$paper_dir/$name" --n_array 23 --memory "$memory" \
-    --wall_time "$wall_time" --trials 100 --split 10 $qsub_dir/$name.qsub
+    --wall_time "$wall_time" --trials 100 --split 10 "$qsub_dir/$name.qsub"
