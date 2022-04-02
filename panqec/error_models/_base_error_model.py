2beac0d17502987afedc0de640ba8e8bfa15b0e1
Arthur Pesah <arthur.pesah@gmail.com>
2022-04-02 13:31:11 +0100

    Make error models qecsim-independent

---
+++ panqec/error_models/_base_error_model.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+from abc import ABCMeta, abstractmethod
+import numpy as np
+from panqec.models import StabilizerCode
+from scipy.sparse import csr_matrix
+
+
+class BaseErrorModel(metaclass=ABCMeta):
+    """Base class for error models"""
+
+    @property
+    @abstractmethod
+    def label(self):
+        """Label used in plots result files
+        E.g. 'PauliErrorModel X1 Y0 Z0'
+        """
+
+    @abstractmethod
+    def generate(self, code: StabilizerCode, probability: float, rng=None) -> csr_matrix:
+        """Generate errors for a given code and probability of failure
+
+        Parameters
+        ----------
+        code : StabilizerCode
+            Errors will be generated on the qubits of the provided code
+        probability: float
+            Physical error rate
+        rng: numpy.random.Generator
+            Random number generator (default=None resolves to numpy.random.default_rng())
+
+        Returns
+        -------
+        error : scipy.sparse.csr_matrix
+            Error as a sparse array of size 1 x 2n (with n the number of qubits)
+            in the binary symplectic format
+        """
+
+    @abstractmethod
+    def probability_distribution(
+        self, code: StabilizerCode, probability: float
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """Probability distribution of X, Y and Z errors on all the qubits of a code
+        Can be used to generate errors and configure decoders
+
+        Parameters
+        ----------
+        code : StabilizerCode
+            Code used for the error model
+        probability: float
+            Physical error rate
+
+        Returns
+        -------
+        p_i, p_x, p_y, p_z : Tuple[np.ndarray]
+            Probability distribution for I, X, Y and Z errors.
+            Each probability is an array of size n (number of qubits)
+        """
--- panqec/error_models/_pauli_error_model.py
+++ panqec/error_models/_pauli_error_model.py
@@ -1,17 +1,28 @@
 import functools
 from typing import Tuple
 import numpy as np
-from qecsim import paulitools as pt
-from qecsim.models.generic import SimpleErrorModel
+import panqec.bsparse as bsparse
 from panqec.models import StabilizerCode
+from . import BaseErrorModel
 
 
-class PauliErrorModel(SimpleErrorModel):
+class PauliErrorModel(BaseErrorModel):
     """Pauli channel IID noise model."""
 
-    # direction: Tuple[float, float, float]
+    def __init__(self, r_x: float, r_y: float, r_z: float):
+        """Initialize Pauli error model at a given rate of X, Y and Z errors,
+        i.e. $P(u) = p * r_u$ for $u \in \{X, Y, Z\}$, $p$ the total error rate, and
+        $P(u)$ the probability of getting the error $u$ on each qubit.
 
-    def __init__(self, r_x, r_y, r_z):
+        Parameters
+        ----------
+        r_x : float
+            Rate of X errors
+        r_y : float
+            Rate of Y errors
+        r_z : float
+            Rate of Z errors
+        """
         if not np.isclose(r_x + r_y + r_z, 1):
             raise ValueError(
                 f'Noise direction ({r_x}, {r_y}, {r_z}) does not sum to 1.0'
@@ -19,7 +30,14 @@ class PauliErrorModel(SimpleErrorModel):
         self._direction = r_x, r_y, r_z
 
     @property
-    def direction(self):
+    def direction(self) -> Tuple[float, float, float]:
+        """Rate of X, Y and Z errors, as given when initializing the error model
+
+        Returns
+        -------
+        (r_x, r_y, r_z): Tuple[float]
+            Rate of X, Y and Z errors
+        """
         return self._direction
 
     @property
@@ -28,17 +46,27 @@ class PauliErrorModel(SimpleErrorModel):
 
     def generate(self, code: StabilizerCode, probability: float, rng=None):
         rng = np.random.default_rng() if rng is None else rng
-        n_qubits = code.n
+
         p_i, p_x, p_y, p_z = self.probability_distribution(code, probability)
 
-        error_pauli = ''.join([rng.choice(
-            ('I', 'X', 'Y', 'Z'),
-            p=[p_i[i], p_x[i], p_y[i], p_z[i]]
-        ) for i in range(n_qubits)])
+        error = bsparse.zero_row(2*code.n)
+        for i in range(code.n):
+            pauli_i = bsparse.zero_row(2*code.n)
 
-        bsf = pt.pauli_to_bsf(error_pauli)
+            pauli_x = bsparse.zero_row(2*code.n)
+            pauli_x[i] = 1
 
-        return bsf
+            pauli_z = bsparse.zero_row(2*code.n)
+            pauli_z[code.n + i] = 1
+
+            pauli_y = bsparse.zero_row(2*code.n)
+            pauli_y[i] = 1
+            pauli_y[code.n + i] = 1
+
+            error += rng.choice([pauli_i, pauli_x, pauli_y, pauli_z],
+                                p=[p_i[i], p_x[i], p_y[i], p_z[i]])
+
+        return error
 
     @functools.lru_cache()
     def probability_distribution(
