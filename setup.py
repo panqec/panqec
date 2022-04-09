3eae1dec10a9cdc26a600fec7d2514b044425813
Arthur Pesah <arthur.pesah@gmail.com>
2022-04-09 18:52:12 +0100

    Add click in requirements and repair bug in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
-requirements = [
-    'PyMatching',
-    'tqdm',
-    'python-dotenv',
-    'pandas',
-    'flask',
-    'ldpc'
-]
+requirements = ["PyMatching==0.2.4",
+                "tqdm==4.59.0",
+                "python-dotenv==0.17.0",
+                "psutil==5.8.0",
+                "ldpc==0.1.0",
+                "click==8.1.2"]
 
 setup(
     name='panqec',
+    version='0.1.2',
+    license='MIT',
     description='Simulation and visualization of quantum error correcting codes',
-    url='http://github.com/ehua7365/panqec',
+    url='http://github.com/panqec/panqec',
     author='Eric Huang',
     email='ehuang1@perimeterinstitute.ca',
     packages=find_packages(),
     zip_safe=False,
-    requirements=requirements,
+    install_requires=requirements,
     scripts=['bin/panqec'],
 )
