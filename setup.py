37a8ad5ddcbfab5687732c6a2cf07d5861ae0a91
Arthur Pesah <arthur.pesah@gmail.com>
2022-04-12 15:54:49 +0100

    Add missing dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
 requirements = ["PyMatching==0.2.4",
+                "numpy==1.22.0",
+                "scipy==1.8.0",
                 "tqdm==4.59.0",
                 "python-dotenv==0.17.0",
+                "networkx==2.8",
                 "psutil==5.8.0",
                 "ldpc==0.1.0",
-                "click==8.1.2"]
+                "click==8.1.2",
+                "Flask==2.1.1",
+                "pandas==1.4.2"]
 
 setup(
     name='panqec',
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='Simulation and visualization of quantum error correcting codes',
     url='http://github.com/panqec/panqec',
