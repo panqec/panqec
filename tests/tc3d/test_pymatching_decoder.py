dafc83a912b25b9785dd49f694c92b241379bbcf
ehua7365 <ehua7365@users.noreply.github.com>
2022-04-30 18:17:01 -0400

    [Change] fix test_pymatching_decoder

--- tests/tc3d/test_pymatching_decoder.py
+++ tests/tc3d/test_pymatching_decoder.py
@@ -23,52 +23,51 @@ class TestToric3DPymatchingDecoder:
         assert decoder.label is not None
         assert decoder.decode is not None
 
-    @pytest.mark.skip(reason='refactor')
     def test_decode_trivial_syndrome(self, decoder, code):
         syndrome = np.zeros(
             shape=code.stabilizer_matrix.shape[0], dtype=np.uint
         )
         correction = decoder.decode(code, syndrome)
-        assert correction.shape == 2*code.n
+        assert correction.shape[0] == 2*code.n
         assert np.all(bcommute(code.stabilizer_matrix, correction) == 0)
         assert issubclass(correction.dtype.type, np.integer)
 
-    @pytest.mark.skip(reason='refactor')
     def test_decode_X_error(self, decoder, code):
-        error = dict()
-        error[(2, 1, 2)] = 'X'
-        assert bsf_wt(code.to_bsf(error)) == 1
+        error = code.to_bsf({
+            (2, 1, 2): 'X',
+        })
+        assert bsf_wt(error) == 1
 
         # Measure the syndrome and ensure non-triviality.
         syndrome = code.measure_syndrome(error)
         assert np.any(syndrome != 0)
 
         correction = decoder.decode(code, syndrome)
-        total_error = (code.to_bsf(error) + correction) % 2
+        total_error = (error + correction) % 2
 
         assert np.all(bcommute(code.stabilizer_matrix, total_error) == 0)
 
-    @pytest.mark.skip(reason='refactor')
     def test_decode_many_X_errors(self, decoder, code):
-        error = dict()
-        error[(1, 0, 0)] = 'X'
-        error[(0, 1, 0)] = 'X'
-        error[(0, 0, 3)] = 'X'
-        assert bsf_wt(code.to_bsf(error)) == 3
+        error = code.to_bsf({
+            (1, 0, 0): 'X',
+            (0, 1, 0): 'X',
+            (0, 0, 3): 'X',
+        })
+        assert bsf_wt(error) == 3
 
         syndrome = code.measure_syndrome(error)
         assert np.any(syndrome != 0)
 
         correction = decoder.decode(code, syndrome)
-        total_error = (code.to_bsf(error) + correction) % 2
+        total_error = (error + correction) % 2
         assert np.all(bcommute(code.stabilizer_matrix, total_error) == 0)
 
-    @pytest.mark.skip(reason='refactor')
     def test_unable_to_decode_Z_error(self, decoder, code):
-        error = dict()
-        error[(1, 0, 2)] = 'Z'
+        error = code.to_bsf({
+            (1, 0, 2): 'Z'
+        })
 
-        assert bsf_wt(code.to_bsf(error)) == 1
+        assert bsf_wt(error) == 1
 
         syndrome = code.measure_syndrome(error)
         assert np.any(syndrome != 0)
@@ -76,12 +75,11 @@ class TestToric3DPymatchingDecoder:
         correction = decoder.decode(code, syndrome)
         assert np.all(correction == 0)
 
-        total_error = (code.to_bsf(error) + correction) % 2
-        assert np.all(code.to_bsf(error) == total_error)
+        total_error = (error + correction) % 2
+        assert np.all(error == total_error)
 
         assert np.any(bcommute(code.stabilizer_matrix, total_error) != 0)
 
-    @pytest.mark.skip(reason='refactor')
     def test_decode_many_codes_and_errors_with_same_decoder(self, decoder):
 
         codes = [
@@ -97,9 +95,10 @@ class TestToric3DPymatchingDecoder:
         ]
 
         for code, site in itertools.product(codes, sites):
-            error = dict()
-            error[site] = 'X'
+            error = code.to_bsf({
+                site: 'X',
+            })
             syndrome = code.measure_syndrome(error)
             correction = decoder.decode(code, syndrome)
-            total_error = (code.to_bsf(error) + correction) % 2
+            total_error = (error + correction) % 2
             assert np.all(bcommute(code.stabilizer_matrix, total_error) == 0)
