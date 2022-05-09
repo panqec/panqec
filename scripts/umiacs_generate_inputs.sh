f7236e1a6edbc6f32b83b4673acd7d10f53ff644
ehua7365 <ehua7365@users.noreply.github.com>
2022-05-09 10:48:05 -0400

    [Change] update parameters

--- scripts/umiacs_generate_inputs.sh
+++ scripts/umiacs_generate_inputs.sh
@@ -27,30 +27,30 @@ panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class PauliErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "3" --prob "0.04:0.10:0.005"
+    --eta "3" --prob "0.10:0.18:0.01"
 
-# estimated p_th = 0.0988
+# estimated p_th = 0.1022
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class PauliErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
     --eta "10" --prob "0.08:0.12:0.005"
 
-# estimated p_th = 0.0962
+# estimated p_th = 0.0988
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class PauliErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
     --eta "30" --prob "0.08:0.12:0.005"
 
-# estimated p_th = 0.0951
+# estimated p_th = 0.0975
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class PauliErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
     --eta "100" --prob "0.08:0.12:0.005"
 
-# estimated p_th unknown
+# estimated p_th = 0.0973
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class PauliErrorModel \
     --ratio equal \
@@ -71,47 +71,47 @@ rm -rf $paper_dir/$name/logs
 sizes="7,9,11,13"
 wall_time="4:59:00" memory="20G"
 
-# estimated p_th unknown
+# estimated p_th = 0.0477
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "0.5" --prob "0.02:0.07:0.005"
+    --eta "0.5" --prob "0.03:0.07:0.005"
 
-# estimated p_th unknown
+# estimated p_th = 0.0743
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "3" --prob "0.01:0.2:0.01"
+    --eta "3" --prob "0.05:0.09:0.005"
 
-# estimated p_th unknown
+# estimated p_th = 0.1121
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "10" --prob "0.02:0.3:0.02"
+    --eta "10" --prob "0.09:0.13:0.005"
 
-# estimated p_th unknown
+# estimated p_th = 12.82
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "30" --prob "0.03:0.50:0.03"
+    --eta "30" --prob "0.11:0.15:0.005"
 
-# estimated p_th unknown
+# estimated p_th unknown but point sector p_th = 0.14
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "100" --prob "0.03:0.50:0.03"
+    --eta "100" --prob "0.12:0.16:0.005"
 
-# estimated p_th unknown
+# estimated p_th unknown but point sector 0.14
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "inf" --prob "0.03:0.50:0.03"
+    --eta "inf" --prob "0.12:0.16:0.03"
 
 panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array 8 \
     --memory "$memory" --qos "$qos" \
