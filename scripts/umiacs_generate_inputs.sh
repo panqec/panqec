3c3f426f2e827c8bcc2c6951cf4dfa3fee5718a2
Eric Huang <ehua7365@users.noreply.github.com>
2022-07-03 15:39:11 -0400

    [Change] each core runs all p

--- scripts/umiacs_generate_inputs.sh
+++ scripts/umiacs_generate_inputs.sh
@@ -94,12 +94,11 @@ panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array $nfiles \
     --wall_time "$wall_time" --trials 10000 --split auto $sbatch_dir/$name.sbatch
 
 # ============== XCube Deformed Inf Only ==============
-name=umiacs_xcube_bposd_xzzx_zbias_inf
+common_name=umiacs_xcube_bposd_xzzx_zbias_inf
+name=${common_name}_temp
 rm -rf $paper_dir/$name/inputs
 rm -rf $paper_dir/$name/logs
 sizes="7,9,11,13"
-wall_time="00:30:00"
-memory="20G"
 
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
@@ -109,6 +108,16 @@ panqec generate-input -i "$paper_dir/$name/inputs" \
 
 nfiles=$(ls $paper_dir/$name/inputs | wc -l)
 echo "$nfiles input files created"
-panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array 8 \
-    --memory "$memory" --qos "$qos" \
-    --wall_time "$wall_time" --trials 1000 --split auto $sbatch_dir/$name.sbatch
+
+for repeat in $(seq 1 8); do
+    name=${common_name}_${repeat}
+    mkdir -p $paper_dir/$name
+    rm -rf $paper_dir/$name/inputs
+    rm -rf $paper_dir/$name/logs
+    cp -R $paper_dir/${common_name}_temp/inputs $paper_dir/$name/inputs
+    panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array $nfiles \
+        --memory "$memory" --qos "$qos" \
+        --wall_time "$wall_time" --trials 125 --split auto $sbatch_dir/$name.sbatch
+done
+
+rm -rf $paper_dir/${common_name}_temp
