5c45d1e27750e8467742f9ae29e1a2de780c5942
Eric Huang <ehua7365@users.noreply.github.com>
2022-07-03 14:58:43 -0400

    [Change] lump json files with all probs in 1

--- scripts/umiacs_generate_inputs.sh
+++ scripts/umiacs_generate_inputs.sh
@@ -97,19 +97,18 @@ panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array $nfiles \
 name=umiacs_xcube_bposd_xzzx_zbias_inf
 rm -rf $paper_dir/$name/inputs
 rm -rf $paper_dir/$name/logs
-sizes="9,13,15"
-# sizes="7,9,11,13"
-wall_time="00:05:00"
+sizes="7,9,11,13"
+wall_time="00:30:00"
 memory="20G"
 
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "inf" --prob "0.05:0.50:0.05"
+    --eta "inf" --prob "0.10:0.20:0.02"
 
 nfiles=$(ls $paper_dir/$name/inputs | wc -l)
 echo "$nfiles input files created"
-panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array $nfiles \
+panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array 8 \
     --memory "$memory" --qos "$qos" \
     --wall_time "$wall_time" --trials 1000 --split auto $sbatch_dir/$name.sbatch
