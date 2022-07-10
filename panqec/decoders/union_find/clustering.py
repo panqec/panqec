e9b863404a2c41fa6ffe4e2c4fa27344bbdcd761
Lynna <lynna.xie@gmail.com>
2022-07-11 00:59:48 +0100

    work out algorithm sturcture for clustering

--- panqec/decoders/union_find/clustering.py
+++ panqec/decoders/union_find/clustering.py
@@ -1,17 +1,39 @@
 from abc import ABCMeta, abstractmethod
 from __future__ import annotations
 from typing import Tuple, List
-
+import numpy as np
 from pyrsistent import s
 
+def clustering(syndrome):
+    vertices: List = transfer_syndrome(syndrome) # turn syndromes into vertices
+    cluster_forest = map_vertex_tree(vertices) # map vertices with cluster information
+    support = Support()
 
+    while vertices:
+        fusion_list = []
+        for u in vertices:
+            fursion_list = grow(cluster_forest(u)) #inlcude step 9
+        
+        for e in fusion_list:
+            if find(e.u) != find(e.v):
+                union(e.u, e.v) #include step 7,8
+            else:
+                fusion_list.remove(e)
+        
+        update_verices(vertices) #remove if true       
+
+    return #grwon supprt and syndrome
+
+def union(vt_v, vt_u, forest):
+    return
 
 class Cluster_Tree():
 
-    def __init__(self):
+    def __init__(self, root):
         self._size_ = 1
-        self._root_: Vertex = None
-        self._boundary_list: List[Vertex] = []
+        self._odd_ = True
+        self._root_: Vertex = root
+        self._boundary_list: List[Vertex] = [root]
 
 class Vertex():
 
@@ -34,3 +56,18 @@ class Vertex():
     
     def get_parent(self):
         return self._parent_
+
+    def find(self) -> Vertex:
+        pass
+
+def Support():
+    UNOCCUPIED = 0
+    HALF_GROWN = 1
+    GROWN      = 2
+
+    def __init__(self, x_len, y_len):
+        self._x_len_ = x_len
+        self._y_len_ = y_len
+        self._edges_ = np.zeros((x_len, y_len), dtype='uint8')
+
+    def grow(vertex):
