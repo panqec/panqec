a16fbf268e988894056c6a8f25381640f85b47f2
Eric Huang <ehua7365@users.noreply.github.com>
2022-07-28 11:02:44 -0400

    [Change] run parameters again

--- scripts/umiacs_generate_inputs.sh
+++ scripts/umiacs_generate_inputs.sh
@@ -6,33 +6,33 @@ mkdir -p temp/paper/share
 qos=dpart
 
 # ============== XCube Deformed ==============
-common_name=det_xcube_bposd_xzzx_zbias_eta30100
+common_name=det_xcube_bposd_xzzx_zbias_eta100
 name=${common_name}_temp
 rm -rf $paper_dir/$name/inputs
 rm -rf $paper_dir/$name/logs
 sizes="13,17,21"
-wall_time="72:00:00"
+wall_time="48:00:00"
 memory="20G"
 
-# p_th unknown probably about 0.10
-panqec generate-input -i "$paper_dir/$name/inputs" \
-    --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
-    --ratio equal \
-    --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "30" --prob "0.08:0.12:0.005"
+# # p_th unknown probably about 0.10
+# panqec generate-input -i "$paper_dir/$name/inputs" \
+#     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
+#     --ratio equal \
+#     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
+#     --eta "30" --prob "0.08:0.12:0.005"
 
 # estimated p_th unknown but point sector p_th = 0.14
 panqec generate-input -i "$paper_dir/$name/inputs" \
     --code_class XCubeCode --noise_class DeformedXZZXErrorModel \
     --ratio equal \
     --sizes "$sizes" --decoder BeliefPropagationOSDDecoder --bias Z \
-    --eta "100" --prob "0.12:0.18:0.005"
+    --eta "100" --prob "0.08:0.18:0.005"
 
 
 nfiles=$(ls $paper_dir/$name/inputs | wc -l)
 echo "$nfiles input files created"
 
-for repeat in $(seq 1 4); do
+for repeat in $(seq 1 8); do
     name=${common_name}_${repeat}
     mkdir -p $paper_dir/$name
     rm -rf $paper_dir/$name/inputs
@@ -40,7 +40,7 @@ for repeat in $(seq 1 4); do
     cp -R $paper_dir/${common_name}_temp/inputs $paper_dir/$name/inputs
     panqec umiacs-sbatch --data_dir "$paper_dir/$name" --n_array $nfiles \
         --memory "$memory" --qos "$qos" \
-        --wall_time "$wall_time" --trials 800 --split auto $sbatch_dir/$name.sbatch
+        --wall_time "$wall_time" --trials 400 --split auto $sbatch_dir/$name.sbatch
 done
 
 rm -rf $paper_dir/${common_name}_temp
