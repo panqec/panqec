3a958bef7e7fcc2b324b0940a95bf0f78b9807d8
Osama Alsaiari <osamaons@gmail.com>
2022-09-08 13:02:08 +0100

    Modification to the peeling decoder step, where a spanning forest of the clusters is created and then it is peeled to obtain the correction for the error. In this modification, a few mistakes in the code has been fixed and parts of the code has been tested. Additionally, the decoder was made to work for any index values used for the nodes (vertix stabilizer indices) and hence it is compatible with panqec indexing. The only thing left to do is to construct graphs for each cluster using the output from the clustering step.

--- panqec/decoders/union_find/uf_decoder.py
+++ panqec/decoders/union_find/uf_decoder.py
@@ -32,87 +32,120 @@ class UnionFindDecoder(BaseDecoder):
         ### clustering/syndrome validation stage. Outputs cluster trees and support -- Lynna
 
 
+        #assume the output is output = [1:(s[0,1,2], q[3,4,5]), 2:(s[22,33,44], q[34,46,57]), ...]
+        # and syndrome = [2,3,4] which is a list of all vertix indices that are syndromes
+
         ### Peeler decoder -- Z error ###
 
-        ## first we construct lattice/grid graphs  for each cluster.-- to do later##
+        ## first we construct lattice/grid graphs  for each cluster ##
         # these graphs are stored as an adjacency list that repeats the edges (since it stores the neighbours of each node)
         # note that if the edges are note repeated this will affect the edge removal in the tree finding function below (then need to add an
-        # extra if function). Note that in each graph the nodes are represented by integer indices from 0 to n-1 (for n nodes) in this order.
+        # extra if function). Note that in each graph the nodes (lattice vertices) are represented by their panqec integer indices 
+        # in the keys of the dictionary, ie Adjacency list format used: {0:[1,2], 1:[0,2], 2:[0,1]} ; 0,1,2 are the indices of the nodes
+
+
+        # constructing the graphs --> Lynna
+
+        graphs = # the final output which is a list of dictionaries
+
+        # the syndromes of each graph as a list of lists called "sig"
+        sig = [] # list of all syndromes organised by cluster
+        for graph in output:
+            vertices_i = graph[0]
+            sig_i = []
+            for vertix in vertices_i:
+                if vertix in syndrome:
+                    sig_i.append(vertix)
+            sig.append(sig_i)
+
+
+
+
+
+
 
 
 
         ## now assuming we have the cluster graphs we find spanning forest ##
-        def nu_nodes(a):
-            """Getting the number of nodes of a graph from
+        def nu_nodes(g):
+            """Getting the number of nodes of a graph from its adjacency list
 
-            Input:  a  Dictionary representing the adjaceny list of the graph
+            Input:  g  Dictionary representing the adjaceny list of the graph
             Output:    Integer representing the number of nodes of the graph                
             """
-            return len(a.keys())
+            return len(g.keys())
 
         def find_neighbours(g,n):
             """Function that finds the neighbour of a node within a graph.
 
             Inputs:
             g the dictionary representing the adjacency list of the graph
-            n the node whose neighbours are to be found
+            n the index of the node whose neighbours are to be found
 
             Output:
-            l a list of the neighbour (indices) of the node n            
+            a list of the indices of the neighbours of the node n            
             """
             return g[n]
 
-        def find_tree(g, s=0):
-                """Function that finds a spanning tree for a graph
+        def find_tree(g):
+                """Function that finds a spanning tree for a graph g. This function uses a BFS algorithm and starts at the first
+                node in the adjaceny dictionary of g (first node means the first key of the dictionary).
                 Input: 
-                g  dictionary representing the adjacency list of the graph for which the spanning tree is to be found
-                s  the node to start the BFS algorithm at. 0 by default
-
+                g      dictionary representing the adjacency list of the graph for which the spanning tree is to be found
+                
                 Output:
-                tree the adjaceny list of the spanning tree of g                
+                tree   the adjaceny list of the spanning tree of g                
                 
                 """
+                tree = g # initialise tree adjacency list
+                nodes = list(tree.keys()) # list of nodes in the tree
 
                 q = [] # empty list representing the que
+                s = nodes[0] # the first key (node) of the tree dictionary
                 q.append(s) # adding s to the que
 
                 n = nu_nodes(g) # number of nodes of g
-                visited = [False]*n # array to check if a node has been visited
+
+                # creating "visited" and "parent" dictionaries which are both initialised as False for all the nodes
+                values = [False]*n                 
+                visited = {nodes[i]:values[i] for i in range (n)} # a dictionary to check if a node has been visited
+                parent = visited # a dictionary to check if a node is a parent node
                 visited[s]= True # mark start node as being visited
+                
+                while len(q): # while the que is not empty
+                    current_node = q[0] # the node at the start of the que
+                    q.pop(0)  # remove "current_node" from the que
+                    parent[current_node] = True # "current_node" is now a parent node
 
-                tree = g # initialise tree adjacency list
+                    for next in find_neighbours(tree,current_node): # looping through the neighbours of "current_node" in the tree
 
-                while len(q)>0 : # while the que is not empty
-                    node = q[0] # the node at the start of the que
-                    q.pop(0)  # remove "node" from the que
+                        if not parent[next]: # if a parent node we do nothing
+                        
+                            if not visited[next]: # if node was not visited before, add to the que
+                                q.append(next)
+                                visited[next] = True                       
 
-                    for next in find_neighbours(g,node): # looping through the neighbours of "node"
-
-                        if !visitied[next]:
-                            q.append(next)
-                            visited[next] = True                       
-
-                            
-                        elif (next in tree[node]): # check if the edge between "next" and "node" exists
-                            # remove the edge between "next" and "node" entirely from the tree graph
-                            tree[node].remove(next)
-                            tree[next].remove(node)
+                                
+                            else: # if node is visited
+                                # remove the edge between "next" and "current_node" entirely from the tree graph
+                                tree[current_node].remove(next)
+                                tree[next].remove(current_node)
 
                 return tree
 
 
                 
-        ## Assuming L is a list/array of the individual cluster graphs (ie list/array of dictionaries), we find the forest:
+        ## Assuming graphs is a list/array of the individual cluster graphs (ie list/array of dictionaries), we find the forest:
 
         forest = [] # initialise the forest list
-        for graph in L:
+        for graph in graphs:
             tree = find_tree(graph)
             forest.append(tree)            
             
 
-        ## Now the peeling the forest ##
+        ## Now peeling the forest ##
 
-        ## assuimung we make a list of syndromes within each cluster with the vertices names 0 to n, called sig_i such that we have all
+        ## we assume that we  have a list of syndromes within each cluster with the vertices named 0 to n, called sig_i such that we have all
         ## syndromes as sig = list of all sig_i. Alternatively fix the indexing of the cluster graphs to be 0 to N, where N is the number of 
         ## nodes in all clusters. If you adopt this indexing, then (maybe) change the way the nodes are indexed in the dictionaries to become 
         ## the key name ++ other changes needed!.
@@ -123,16 +156,16 @@ class UnionFindDecoder(BaseDecoder):
         A = []  # list of edges to be corrected
         for i,tree in enumerate(forest):
             sig_i = sig[i] # pick the syndromes list for the appropriate cluster
-            leaves = [] # list of leaf nodes (pendant vertices)
+            leaves = [] # list of leaf nodes, the pendant vertices
             for node in tree.keys(): # append node to "leaves" if it has one neighbour
                 if len(tree[node])==1: 
                     leaves.append(node)                     
 
-            while len(tree)>0: # while tree is not empty (not fully peeled)
-                v = leaves[0] # pendant vertex chosen randomly (first element of "leaves")
-                u = tree[v][0] # neighbour of the pendant vertex
+            while len(tree.keys()): # while tree is not empty 
+                v = leaves[0] # choose pendant vertex to work with randomly (first element of "leaves")
+                u = tree[v][0] # neighbour of the pendant vertix, which is the vertix connecting the leaf edge to the forest
 
-                ## remove leaf v from "leaves" and from the tree.
+                ## remove leaf edge from "leaves" and from the tree.
                 leaves.remove(v) # remove v from leaves
                 tree.pop(v) # remove v from the tree
                 tree[u].remove(v) # remove v from the neighbours list of u
@@ -142,11 +175,11 @@ class UnionFindDecoder(BaseDecoder):
                     leaves.append(u)
 
                 ## peeler algorithm
-
+                # this requires sig_i to have the cluster indices of the syndrome vertics
                 if u in sig_i: 
                     e = (u,v) # edge to be corrected
-                    A.append(e)
-                    sig_i.remove(u)
+                    A.append(e)  
+                    sig_i.remove(u) 
                     if v in sig_i:
                         sig_i.remove(v)
                     else:
