390fadd80bcfc8ea7c2677d41326e3855fe24477
Arthur Pesah <arthur.pesah@gmail.com>
2022-09-13 00:18:13 +0100

    X-Cube decoder on all projection axes

--- panqec/decoders/matching/_xcube_matching_decoder.py
+++ panqec/decoders/matching/_xcube_matching_decoder.py
@@ -1,3 +1,4 @@
+from tkinter import E
 import numpy as np
 from typing import Optional, Tuple
 from pymatching import Matching
@@ -5,6 +6,65 @@ from panqec.decoders import BaseDecoder, MatchingDecoder
 from panqec.codes import StabilizerCode, Toric2DCode
 from panqec.decoders.bposd.bposd_decoder import BeliefPropagationOSDDecoder
 from panqec.error_models import PauliErrorModel
+
+
+def get_matched_pairs(H, correction, syndrome):
+    pairs = []
+    seen_syndromes = set([])
+    syndrome_indices = np.nonzero(syndrome)[0]
+
+    for s in syndrome_indices:
+        if s not in seen_syndromes:
+            # print("s", s)
+            seen_syndromes.add(s)
+
+            s_prime = s
+            continue_search = True
+            prev_qubit = -1
+
+            while continue_search:
+                found_new_qubit = False
+                for q in H[s_prime].nonzero()[1]:
+                    # print("q", q)
+                    if correction[q] and q != prev_qubit:
+                        # print("Found new qubit")
+                        found_new_qubit = True
+                        prev_qubit = q
+                        for i in H[:, q].nonzero()[0]:
+                            if i != s_prime:
+                                s_prime = i
+                                break
+
+                        break
+
+                if not found_new_qubit:
+                    continue_search = False
+                    pairs.append((s, s_prime))
+                    seen_syndromes.add(s_prime)
+
+    return pairs
+
+
+def find_connected_components(neighbors):
+    connected_components = []
+    seen = set()
+
+    def component(node):
+        nodes_in_component = set([node])
+        nodes = set([node])
+        while nodes:
+            node = nodes.pop()
+            seen.add(node)
+            nodes.update(neighbors[node] - seen)
+            nodes_in_component.add(node)
+
+        return list(nodes_in_component)
+
+    for node in neighbors:
+        if node not in seen:
+            connected_components.append(component(node))
+
+    return connected_components
 
 
 def modular_average(numbers, weights, n):
@@ -72,6 +132,35 @@ def decode_plane(loops, size):
     return correction_coordinates
 
 
+def get_toric_loop(xcube_matching_ortho, component, proj_axis_int):
+    toric_loop_dict = {}
+    for loc in xcube_matching_ortho:
+        if loc[proj_axis_int] in component:
+            loc_2d = tuple_remove(loc, proj_axis_int)
+
+            if loc_2d in toric_loop_dict.keys():
+                toric_loop_dict[loc_2d] += 1
+            else:
+                toric_loop_dict[loc_2d] = 1
+
+    toric_loop = [coord for coord in toric_loop_dict.keys()
+                  if toric_loop_dict[coord] % 2 == 1]
+
+    return toric_loop
+
+
+def tuple_insert(t, index, element):
+    new_t = list(t)
+    new_t.insert(index, element)
+    return tuple(new_t)
+
+
+def tuple_remove(t, index):
+    new_t = list(t)
+    new_t.pop(index)
+    return tuple(new_t)
+
+
 if __name__ == "__main__":
     avg = modular_average([1, 3, 5, 7], [0.5, 0.0, 0.5, 0.0], 8)
 
@@ -105,6 +194,39 @@ class XCubeMatchingDecoder(BaseDecoder):
         """
         super().__init__(code, error_model, error_rate)
 
+        Lx, Ly, Lz = code.size
+
+        # print("Initialize toric codes")
+        self.toric_code = {'x': Toric2DCode(Ly, Lz),
+                           'y': Toric2DCode(Lx, Lz),
+                           'z': Toric2DCode(Lx, Ly)
+                           }
+
+        rx, ry, rz = self.error_model.direction
+
+        # Weight the 2D toric code matching decoders
+        # Only works for Z biased noise and z-axis deformation
+        # print("Get weights")
+        weights_X, _ = self.error_model.get_weights(self.code,
+                                                    self.error_rate)
+
+        wz = weights_X[self.code.qubit_index[(0, 0, 1)]]
+        wxy = weights_X[self.code.qubit_index[(1, 0, 0)]]
+
+        weights = {'x': [wxy if self.toric_code['x'].qubit_axis(loc) == 'x' else wz
+                         for loc in self.toric_code['x'].qubit_coordinates],
+                   'y': [wxy if self.toric_code['y'].qubit_axis(loc) == 'x' else wz
+                         for loc in self.toric_code['y'].qubit_coordinates],
+                   'z': [wxy for _ in self.toric_code['z'].qubit_coordinates]}
+
+        # print("Initialize matching decoders")
+        self.matching_decoder = {axis: MatchingDecoder(self.toric_code[axis],
+                                                       self.error_model,
+                                                       self.error_rate,
+                                                       weights=(weights[axis],
+                                                                weights[axis]))
+                                 for axis in ['x', 'y', 'z']}
+
         self.z_decoder = BeliefPropagationOSDDecoder(self.code,
                                                      self.error_model,
                                                      self.error_rate)
@@ -115,151 +237,198 @@ class XCubeMatchingDecoder(BaseDecoder):
         # Initialize correction as full bsf.
         correction = np.zeros(2*self.code.n, dtype=np.uint)
 
+        possible_correction = {'x': np.zeros(2*self.code.n, dtype=np.uint),
+                               'y': np.zeros(2*self.code.n, dtype=np.uint),
+                               'z': np.zeros(2*self.code.n, dtype=np.uint)}
+
         Lx, Ly, Lz = self.code.size
 
-        toric_code = {'x': Toric2DCode(Ly, Lz),
-                      'y': Toric2DCode(Lx, Lz),
-                      'z': Toric2DCode(Lx, Ly)
-                      }
-
-        rx, ry, rz = self.error_model.direction
-
-        # Weight the 2D toric code matching decoders
-        # Only works for Z biased noise and z-axis deformation
-        weights_X, _ = self.error_model.get_weights(self.code,
-                                                    self.error_rate)
-
-        wz = weights_X[self.code.qubit_index[(0, 0, 1)]]
-        wxy = weights_X[self.code.qubit_index[(1, 0, 0)]]
-
-        weights = {'x': [wxy if toric_code['x'].qubit_axis(loc) == 'x' else wz
-                         for loc in toric_code['x'].qubit_coordinates],
-                   'y': [wxy if toric_code['y'].qubit_axis(loc) == 'x' else wz
-                         for loc in toric_code['y'].qubit_coordinates],
-                   'z': [wxy for _ in toric_code['z'].qubit_coordinates]}
-
-        decoder = {axis: MatchingDecoder(toric_code[axis],
-                                         self.error_model,
-                                         self.error_rate,
-                                         weights=(weights[axis],
-                                                  weights[axis]))
-                   for axis in ['x', 'y', 'z']}
-
-        plane_syndrome = {'x': {x: np.zeros(toric_code['x'].n_stabilizers)
+        # print("Initialize plane syndrome")
+        plane_syndrome = {'x': {x: np.zeros(self.toric_code['x'].n_stabilizers)
                                 for x in range(1, 2*Lx, 2)},
-                          'y': {y: np.zeros(toric_code['y'].n_stabilizers)
+                          'y': {y: np.zeros(self.toric_code['y'].n_stabilizers)
                                 for y in range(1, 2*Ly, 2)},
-                          'z': {z: np.zeros(toric_code['z'].n_stabilizers)
+                          'z': {z: np.zeros(self.toric_code['z'].n_stabilizers)
                                 for z in range(1, 2*Lz, 2)}
                           }
 
         # Remove X stabilizer syndrome and keep it for later
         x_syndrome = self.code.extract_x_syndrome(syndrome)
         syndrome[self.code.x_indices] = 0
+        n_excitations = np.sum(syndrome)
+        axis_to_int = {'x': 0, 'y': 1, 'z': 2}
 
-        for i_stab in range(len(self.code.stabilizer_index)):
-            if syndrome[i_stab]:
-                x, y, z = self.code.stabilizer_coordinates[i_stab]
+        for proj_axis in ['x', 'y', 'z']:
+            proj_axis_int = axis_to_int[proj_axis]
 
-                idx_face_toric = toric_code['x'].stabilizer_index[(y, z)]
-                plane_syndrome['x'][x][idx_face_toric] = 1
+            ortho_axes = tuple_remove(('x', 'y', 'z'), proj_axis_int)
 
-                idx_face_toric = toric_code['y'].stabilizer_index[(x, z)]
-                plane_syndrome['y'][y][idx_face_toric] = 1
+            L_proj = [Lx, Ly, Lz][proj_axis_int]
 
-                idx_face_toric = toric_code['z'].stabilizer_index[(x, y)]
-                plane_syndrome['z'][z][idx_face_toric] = 1
+            # print("Determine plane syndrome")
+            for i_stab in range(len(self.code.stabilizer_index)):
+                if syndrome[i_stab]:
+                    loc = self.code.stabilizer_coordinates[i_stab]
 
-        # Decode all the 2D toric codes
-        xcube_matching_z = []
-        xcube_matching_xy = []
-        for axis in ['x', 'y', 'z']:
-            for plane_id in plane_syndrome[axis].keys():
-                current_syndrome = plane_syndrome[axis][plane_id]
+                    for axis in ['x', 'y', 'z']:
+                        loc_2d = tuple_remove(loc, axis_to_int[axis])
+                        idx_face = self.toric_code[axis].stabilizer_index[loc_2d]
+                        plane = loc[axis_to_int[axis]]
+                        plane_syndrome[axis][plane][idx_face] = 1
 
-                toric_matching = decoder[axis].decode(current_syndrome)
 
-                for i in range(toric_code[axis].n):
-                    if toric_matching[toric_code[axis].n+i]:
-                        x, y = toric_code[axis].qubit_coordinates[i]
-                        if axis == 'z':
-                            xcube_matching_z.append((x, y, plane_id))
-                        elif axis == 'x':
-                            xcube_matching_xy.append((plane_id, x, y))
-                        elif axis == 'y':
-                            xcube_matching_xy.append((x, plane_id, y))
+            # Decode all the 2D toric codes
+            # print("Decode all the 2D codes")
+            xcube_matching = {'x': [], 'y': [], 'z': []}
+            connected_planes = {plane: set()
+                                for plane in plane_syndrome[proj_axis].keys()}
 
-        # Find all the projection planes
-        planes_connected = {z: set([z]) for z in plane_syndrome['z'].keys()}
+            for axis in ['x', 'y', 'z']:
+                for plane in plane_syndrome[axis].keys():
+                    current_syndrome = plane_syndrome[axis][plane]
 
-        for (x, y, z) in xcube_matching_xy:
-            # print("xy Matching", (x, y, z))
-            if z % 2 == 0:
-                planes_connected[(z - 1) % (2*Lz)].add((z + 1) % (2*Lz))
-                planes_connected[(z + 1) % (2*Lz)].add((z - 1) % (2*Lz))
+                    toric_X_syndrome = self.toric_code[axis].extract_x_syndrome(
+                        current_syndrome
+                    )
 
-        # print(planes_connected)
+                    if np.all(toric_X_syndrome == 0):
+                        continue
 
-        # Warning: does not work (need to merge connections)
-        planes_seen = set([])
-        list_z_proj = []
-        for z_proj in planes_connected.keys():
-            if z_proj not in planes_seen:
-                list_z_proj.append(z_proj)
-                planes_seen.update(planes_connected[z_proj])
+                    toric_matching = self.matching_decoder[axis].decode(
+                        current_syndrome
+                    )
+                    toric_Z_correction = toric_matching[self.toric_code[axis].n:]
+
+                    toric_pairs = get_matched_pairs(
+                        self.toric_code[axis].Hx.todense(),
+                        toric_Z_correction,
+                        toric_X_syndrome
+                    )
+
+                    for i in toric_Z_correction.nonzero()[0]:
+                        x, y = self.toric_code[axis].qubit_coordinates[i]
+                        loc_3d = tuple_insert((x, y), axis_to_int[axis], plane)
+                        xcube_matching[axis].append(loc_3d)
+
+                    if axis != proj_axis:
+                        for pair in toric_pairs:
+                            i1, i2 = pair
+                            loc1 = self.toric_code[axis].qubit_coordinates[i1]
+                            loc2 = self.toric_code[axis].qubit_coordinates[i2]
+
+                            # print("Axis", axis)
+                            # print("Pair", loc1, loc2)
+
+                            proj_component = {'x': {'y': 0, 'z': 0},
+                                            'y': {'x': 0, 'z': 1},
+                                            'z': {'x': 1, 'y': 1}
+                                            }[proj_axis][axis]
+                            plane1 = loc1[proj_component]
+                            plane2 = loc2[proj_component]
+
+                            if plane1 != plane2:
+                                if proj_component == 1:
+                                    connected_planes[plane1+1].add(plane2+1)
+                                    connected_planes[plane2+1].add(plane1+1)
+                                else:
+                                    connected_planes[plane1].add(plane2)
+                                    connected_planes[plane2].add(plane1)
+
+            xcube_matching_proj = xcube_matching[proj_axis]
+            xcube_matching_ortho = set((xcube_matching[ortho_axes[0]]
+                                        + xcube_matching[ortho_axes[1]]))
+
+            # print("XCube matching", xcube_matching)
+            # print("Connected planes", connected_planes)
+
+            # print("Connected components", connected_planes)
+
+            connected_components = find_connected_components(connected_planes)
+
+            # print("Find and perform projections")
+            list_plane_proj = []
+            for component in list(connected_components):
+                # Find the optimal projection plane
+                # n_excitations_plane = {z: np.sum(plane_syndrome['z'][z])
+                #                        for z in plane_syndrome['z'].keys()}
+
+                # average_z = 0
+
+                # if n_excitations != 0:
+                #     weights = [n_excitations_plane[z] / n_excitations
+                #                for z in component]
+
+                #     average_z = modular_average(component, weights, 2*Lz)
+
+                # plane_proj = int(2 * np.floor(average_z/2) + 1)
+
+                plane_proj = component[0]
+                list_plane_proj.append(plane_proj)
 
                 # Perform the projection
-                for (x, y, z) in xcube_matching_z:
-                    if z in planes_connected[z_proj]:
-                        if 0 < z - z_proj <= Lz or 2*Lz + z - z_proj <= Lz:
-                            if z_proj < z:
-                                z_stop = z + 1
+                for loc in xcube_matching_proj:
+                    # print("Z matching", (x, y, z))
+                    plane = loc[proj_axis_int]
+                    ortho_components = tuple_remove(loc, proj_axis_int)
+                    if plane in component:
+                        if (0 < plane - plane_proj <= L_proj or
+                                2*L_proj + plane - plane_proj <= L_proj):
+                            if plane_proj < plane:
+                                plane_stop = plane + 1
                             else:
-                                z_stop = 2*Lz + z + 1
+                                plane_stop = 2*L_proj + plane + 1
 
-                            for z_op in range(z_proj+1, z_stop, 2):
-                                loc = (x, y, z_op % (2*Lz))
+                            for plane_op in range(plane_proj+1, plane_stop, 2):
+                                loc = tuple_insert(ortho_components,
+                                                proj_axis_int,
+                                                plane_op % (2*L_proj))
+
                                 idx = self.code.qubit_index[loc]
-                                correction[idx] += 1
-                        elif z != z_proj:
-                            if z < z_proj:
-                                z_stop = z_proj + 1
+                                possible_correction[proj_axis][idx] += 1
+                        elif plane != plane_proj:
+                            if plane < plane_proj:
+                                plane_stop = plane_proj + 1
                             else:
-                                z_stop = 2*Lz + z_proj + 1
+                                plane_stop = 2*L_proj + plane_proj + 1
 
-                            for z_op in range(z + 1, z_stop, 2):
-                                loc = (x, y, z_op % (2*Lz))
+                            for plane_op in range(plane + 1, plane_stop, 2):
+                                loc = tuple_insert(ortho_components,
+                                                proj_axis_int,
+                                                plane_op % (2*L_proj))
+
                                 idx = self.code.qubit_index[loc]
-                                correction[idx] += 1
+                                possible_correction[proj_axis][idx] += 1
 
-        for z_proj in list_z_proj:
+            # print("Find loops")
             # Find the loops
-            toric_loop_dict = {}
-            for (x, y, z) in xcube_matching_xy:
-                if z in planes_connected[z_proj]:
-                    # print("xy matching", (x, y, z))
-                    if (x, y) in toric_loop_dict.keys():
-                        toric_loop_dict[(x, y)] += 1
-                    else:
-                        toric_loop_dict[(x, y)] = 1
+            for component in connected_components:
+                toric_loop = get_toric_loop(
+                    xcube_matching_ortho, component, proj_axis_int
+                )
+                correction_coordinates = decode_plane(toric_loop, (Lx, Ly))
 
-            toric_loop = [coord for coord in toric_loop_dict.keys()
-                          if toric_loop_dict[coord] % 2 == 1]
+                plane_proj = component[0]
 
-            # print("Toric loop", toric_loop)
+                for loc_2d in correction_coordinates:
+                    loc_3d = tuple_insert(loc_2d, proj_axis_int, plane_proj)
+                    idx = self.code.qubit_index[loc_3d]
 
-            correction_coordinates = decode_plane(toric_loop, (Lx, Ly))
+                    possible_correction[proj_axis][idx] = 1
 
-            for (x, y) in correction_coordinates:
-                idx = self.code.qubit_index[(x, y, z_proj)]
-                correction[idx] = 1
+        weight = [np.sum(c) for c in possible_correction.values()]
+        index_min_weight = np.argmin(weight)
 
+        correction = possible_correction[{0: 'x', 1: 'y', 2: 'z'}[index_min_weight]]
+
+        # print(weight)
+        # print(np.sum(correction))
+        # print("Done")
         # Decode z part
-        syndrome[self.code.z_indices] = 0
-        syndrome[self.code.x_indices] = x_syndrome
+        # syndrome[self.code.z_indices] = 0
+        # syndrome[self.code.x_indices] = x_syndrome
 
-        z_correction = self.z_decoder.decode(syndrome).astype('uint8')
+        # # z_correction = self.z_decoder.decode(syndrome).astype('uint8')
 
-        correction += z_correction
+        # correction += z_correction
 
         return correction % 2
