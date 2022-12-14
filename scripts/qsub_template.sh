44ed8e82a0c2eec4147b4ad4cbb95e8c9a79d8bc
artix41 <arthur.pesah@gmail.com>
2022-12-14 15:45:29 +0000

    Bugs in cluster script

--- scripts/qsub_template.sh
+++ scripts/qsub_template.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 # Request ten minutes of wallclock time (format hours:minutes:seconds).
-#$ -l h_rt=24:00:00
-#$ -l mem=4G
+#$ -l h_rt=00:10:00
+#$ -l mem=2G
 #$ -l tmpfs=1G
 #$ -t 1-5
 #$ -N toric-2d-code
-#$ -pe smp 20
+#$ -pe smp 8
 #$ -wd /home/ucapacp/Scratch/output
 #Local2Scratch
 
@@ -19,8 +19,8 @@ conda activate panqec
 # Variables to change.
 data_dir='temp/toric-2d-code'
 delete_existing=true
-trials=10000
-n_cores=20
+trials=1000
+n_cores=8
 n_nodes=5
 i_node=$SGE_TASK_ID
 
@@ -33,7 +33,7 @@ run_command="panqec run-parallel -d $experiment_dir -n $n_nodes -j $i_node -c $n
 if $delete_existing; then
     run_command=run_command" --delete-existing";
 fi
-monitor_command="panqec monitor "$log_dir/usage_${JOB_ID}_${i_node}.txt" &"
+monitor_command="panqec monitor-usage "$log_dir/usage_${JOB_ID}_${i_node}.txt" &"
 
 echo "Node: $i_node / $n_nodes"
 
