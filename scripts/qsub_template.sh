51f028854dce8d1cd34c21355ce9804407e2b2b9
Arthur Pesah <arthur.pesah@gmail.com>
2022-12-14 19:35:46 +0000

    Repair bug in cluster script

--- scripts/qsub_template.sh
+++ scripts/qsub_template.sh
@@ -19,7 +19,7 @@ conda activate panqec
 # Variables to change.
 data_dir='temp/toric-2d-code'
 delete_existing=true
-trials=1000
+n_trials=1000
 n_cores=8
 n_nodes=5
 i_node=$SGE_TASK_ID
@@ -28,16 +28,17 @@ i_node=$SGE_TASK_ID
 log_dir="$data_dir/logs"
 mkdir -p $log_dir
 
-# The bash command to parallelize.
-run_command="panqec run-parallel -d $experiment_dir -n $n_nodes -j $i_node -c $n_cores -t $n_trials"
+delete_option=""
 if $delete_existing; then
-    run_command=run_command" --delete-existing";
+    delete_option="--delete-existing";
 fi
-monitor_command="panqec monitor-usage "$log_dir/usage_${JOB_ID}_${i_node}.txt
 
 echo "Node: $i_node / $n_nodes"
 
-$monitor_command &
-$bash_command
+# Run monitoring
+panqec monitor-usage "$log_dir/usage_${JOB_ID}_${i_node}.txt" &
+
+# Run PanQEC in parallel on all the cores of the current node
+panqec run-parallel -d $data_dir -n $n_nodes -j $i_node -c $n_cores -t $n_trials $delete_option
 
 date
