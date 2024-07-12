28e018385fd52d4cf1b9f315ba31075d54882d30
Arthur Pesah <arthur.pesah@gmail.com>
2024-07-12 13:15:26 +0100

    Change default code to correct Haah code label

--- panqec/gui/js/main.js
+++ panqec/gui/js/main.js
@@ -5,7 +5,7 @@ import { GUI } from 'https://cdn.skypack.dev/three@0.130.0/examples/jsm/libs/dat
 
 import { TopologicalCode } from './topologicalCode.js';
 
-var defaultCode = codeDimension == 2 ? 'Toric 2D' : "Haah's code";
+var defaultCode = codeDimension == 2 ? 'Toric 2D' : "Haah code";
 var defaultSize = codeDimension == 2 ? 6 : 4;
 
 const params = {
